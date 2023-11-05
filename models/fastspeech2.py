a066afcc23dfd03bb775c3ae438ec7132395e164
Tiexin Liu <67965439+sfezn1220@users.noreply.github.com>
2023-11-05 23:51:02 +0800

    搭建conformer的相对位置编码模块

--- models/fastspeech2.py
+++ models/fastspeech2.py
@@ -15,6 +15,11 @@ class ConformerBlock(nn.Module):
         self.end_feed_forward = FeedForwardModule()
 
     def forward(self, x, mask):
+        """
+        :param x: [batch, in_channel, time]
+        :param mask: [batch, 1, time]
+        :return: [batch, out_channel, time]
+        """
 
         x = self.start_feed_forward(x)
         x = self.multi_head_self_attention(x, mask)
@@ -33,40 +38,149 @@ class MultiHeadSelfAttention(nn.Module):
 
         self.layer_norm = nn.LayerNorm(in_out_channel)
 
-        self.head_nums = head_nums
         self.channel = in_out_channel
-        self.linear_q = nn.Linear(in_features=in_out_channel, out_features=in_out_channel)
-        self.linear_k = nn.Linear(in_features=in_out_channel, out_features=in_out_channel)
+        self.head_nums = head_nums
+        self.head_size = in_out_channel // head_nums
+        self.linear_q = nn.Linear(in_features=in_out_channel, out_features=self.head_size * head_nums)
+        self.linear_k = nn.Linear(in_features=in_out_channel, out_features=self.head_size * head_nums)
         self.linear_v = nn.Linear(in_features=in_out_channel, out_features=in_out_channel)
+
+        self.linear_pos_emb = nn.Linear(in_features=in_out_channel, out_features=self.head_size * head_nums)
+
+        self.bias_u = nn.Parameter(torch.Tensor(1, head_nums, 1, self.head_size))
+        self.bias_v = nn.Parameter(torch.Tensor(1, head_nums, 1, self.head_size))
+
         self.dropout = nn.Dropout(dropout_rate)
+
+        self.linear_output = nn.Linear(in_features=in_out_channel, out_features=in_out_channel)
+
+    @staticmethod
+    def get_position_embedding(x):
+        """
+        生成 相对位置编码所需的 position embedding；
+        :param x: [batch, channel, time]
+        :return: [batch, channel, time]
+        """
+        batch, channel, time = x.shape
+
+        # 先生成一个 [time, channel] 的系数矩阵：
+        factor = torch.arange(0, channel, 2, dtype=x.dtype).unsqueeze(1)  # [channel // 2, 1]
+        factor = torch.pow(torch.tensor(10000.0), -1 * factor) * channel
+        position = torch.arange(0, time, dtype=x.dtype).unsqueeze(0)  # [1, time]
+        position = position * factor  # [channel // 2, time]
+
+        # 将position分成两部分，分别初始化：
+        position_positive = torch.zeros(channel, time, dtype=x.dtype)  # [channel, time]
+        position_negative = torch.zeros(channel, time, dtype=x.dtype)  # [channel, time]
+
+        # 计算position的两个部分：
+        position_positive[0::2, :] = torch.sin(position)
+        position_positive[1::2, :] = torch.cos(position)
+        position_negative[0::2, :] = torch.sin(-1 * position)
+        position_negative[1::2, :] = torch.cos(-1 * position)
+
+        # 拼接出position embedding
+        position_positive = torch.flip(position_positive, dims=[1])  # [channel, time]
+        position_negative = position_negative[:, 1:]  # [channel, times-1]
+        position_embedding = torch.cat([position_positive, position_negative], dim=1)  # [channel, time * 2 - 1]
+        position_embedding = position_embedding.unsqueeze(0)  # [1, channel, time * 2 - 1]
+
+        return position_embedding.to(x.device)
 
     def calculate_qkv(self, x):
         """ [batch, channels, time] -> [batch, head_nums * head_size, time] -> [batch, head_nums, time, head_size] """
         batch_size = x.shape[0]
         time = x.shape[2]
 
-        queue = self.linear_q(x.transpose(1, 2)).transpose(1, 2)  # nn.Linear 要求 channel last
+        query = self.linear_q(x.transpose(1, 2)).transpose(1, 2)  # nn.Linear 要求 channel last
         key = self.linear_k(x.transpose(1, 2)).transpose(1, 2)
         value = self.linear_v(x.transpose(1, 2)).transpose(1, 2)
 
-        queue = queue.view([batch_size, self.head_nums, x, -1])
-        key = key.view([batch_size, self.head_nums, x, -1])
-        value = value.view([batch_size, self.head_nums, x, -1])
+        query = torch.reshape(query, [batch_size, self.head_nums, time, -1])
+        key = torch.reshape(key, [batch_size, self.head_nums, time, -1])
+        value = torch.reshape(value, [batch_size, self.head_nums, time, -1])
 
-        return queue, key, value
+        return query, key, value  # [batch, head_nums, time, head_size]
+
+    @staticmethod
+    def relative_shift(matrix):
+        """
+        计算相对位置编码；
+        :param matrix: [batch, head_nums, time, time * 2 - 1]
+        :return: [batch, head_nums, time, time]
+        """
+        batch, heads, time, _ = matrix.shape
+
+        zeros_pad = torch.zeros([batch, heads, time, 1], dtype=matrix.dtype, device=matrix.device)
+        matrix_padded = torch.cat([zeros_pad, matrix], dim=-1)
+        matrix_padded = torch.reshape(matrix_padded, [batch, heads, -1, time])
+
+        matrix = matrix_padded[:, :, 1:, :]
+        matrix = torch.reshape(matrix, [batch, heads, time, -1])
+
+        return matrix[:, :, :, :time]
+
+    def calculate_matrix(self, position_embedding, query, key):
+        """
+        计算：基于相对位置编码的 attention 打分结果；
+        :param position_embedding: [1, channel, time * 2 - 1]
+        :param query: [batch, head_nums, time, head_size]
+        :param key: [batch, head_nums, time, head_size]
+        :return: attention_score: [batch, head_nums, time, time]
+        """
+        position_embedding = self.linear_pos_emb(position_embedding.transpose(1, 2)).transpose(1, 2)  # [1, head_nums * head_size, time*2-1]
+        position_embedding = torch.reshape(position_embedding, [1, self.head_nums, -1, self.head_size])  # [1, head_nums, time*2-1, head_size]
+
+        query_with_u = query + self.bias_u  # [batch, head_nums, time, head_size]
+        query_with_v = query + self.bias_v  # [batch, head_nums, time, head_size]
+
+        matrix_ac = torch.matmul(query_with_u, key.transpose(2, 3))  # [batch, head_nums, time, time]
+        matrix_bd = torch.matmul(query_with_v, position_embedding.transpose(2, 3))  # [batch, head_nums, time, time*2-1]
+        matrix_bd = self.relative_shift(matrix_bd)  # [batch, head_nums, time, time]
+
+        return matrix_bd + matrix_ac
+
+    def calculate_masked_attention_score(self, attention_score, mask, value):
+        """
+        对输入的 attention_score 进行 mask；
+        :param attention_score: [batch, channel, time, time]
+        :param mask: [batch, 1, time]
+        :param value: [batch, channel, time, time]
+        :return: [batch, channel, time]
+        """
+        batch, channel, time, _ = attention_score.shape
+
+        attention_score /= torch.sqrt(attention_score)  # 除以根号下文本长度
+
+        mask = mask.unsqueeze(dim=-1)  # [batch, 1, time, 1]
+        mask = mask.eq(0)
+
+        attention_score = attention_score.masked_fill(mask, -1e9)  # 将mask=0位置的分数，设置为很小的值
+
+        attention_probs = torch.softmax(attention_score, dim=-1)
+        attention_probs = attention_probs.masked_fill(mask, 0.0)  # 将mask=0位置的概率，设置为零
+        attention_probs = self.dropout(attention_probs)
+
+        outputs = torch.matmul(attention_probs, value)  # [batch, channel, time, time]  # TODO: 解决 att_probs 和 value 维度对不上的问题
+        outputs = outputs.transpose(1, 2)
+        outputs = torch.reshape(outputs, [batch, time, channel])
+        outputs = self.linear_output(outputs)
+
+        return outputs.transpose(1, 2)  # [batch, channel, time]
 
     def forward(self, x, mask):
         """
         :param x: [batch, in_out_channel, time]
+        :param mask: [batch, 1, time]
         :return: [batch, in_out_channel, time]
         """
         residual = nn.Identity()(x)
         x = self.layer_norm(x.transpose(1, 2)).transpose(1, 2)  # layer norm 需要 channel last
         # self-attention
-        queue, key, value = self.calculate_qkv(x)
-        position_embedding = self.calculate_postion_embdding(x)  # TODO: 生成相对位置编码的emb，并进行卷积
-        attention_score = self.calculate_maxtrix(x)  # TODO: 计算相对位置编码所需的 matrix
-        x = self.calculate_masked_attention_score(x)  # TODO: 计算attention score
+        query, key, value = self.calculate_qkv(x)
+        position_embedding = self.get_position_embedding(x)
+        attention_score = self.calculate_matrix(position_embedding, query, key)
+        x = self.calculate_masked_attention_score(attention_score, mask, value)
 
         x = self.dropout(x)
         x += residual
@@ -178,7 +292,7 @@ class ConformerEncoder(nn.Module):
     def forward(self, x: torch.tensor, mask: torch.tensor):
         """
         :param x: [batch, in_channel, time]
-        :param mask: 与 x 尺寸一致
+        :param mask: [batch, 1, time]
         :return: [batch, out_channel, time]
         """
         # 检查尺寸
